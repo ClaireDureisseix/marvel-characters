4b2ca309e4d4a6b8a862610f2917df05152fd0c4
ClaireWeb <49585161+ClaireWeb@users.noreply.github.com>
2019-12-06 13:20:08 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 import md5 from 'md5';
 
 const publicKey = `194e82fa7752a06e318949ee25879c5d`;
-const privateKey = `f35ebf82567f98d0388fbd8b713236fbd81027ca`;
 const ts = 1;
-const hash = md5(ts + privateKey + publicKey);
+const hash = '52e8b131792ad4542ccc9adab79033f9';
 
 const opts = `characters`; // whatever parameters you want, e.g., `characters/1009215`.
 const url = `http://gateway.marvel.com/v1/public/${opts}`; // putting it all together
