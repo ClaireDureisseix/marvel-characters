f3b3410ef882279b9bc55fbd3869408a4af5356e
Claire <clairepelletier.prod@gmail.com>
2019-12-06 16:46:03 +0100

    DONE: show page

--- src/components/CharacterShow.js
+++ src/components/CharacterShow.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
 import Title from './Title'
 import apiId from '../apiId';
@@ -10,17 +13,12 @@ class CharacterShow extends Component {
 
   componentDidMount() {
     this.fetchCharacter()
-    
-    
   }
+
   fetchCharacter = () => {
-    const id = this.props.match.params.id;
-    console.log(id);
-    
+    const id = this.props.match.params.id;    
     apiId(id).then(res => {
-      console.log(res.data.data.results[0]);
       this.setState({ character: res.data.data.results[0] })
-      console.log('state', this.state.character);
     })
   }
 
@@ -30,8 +28,63 @@ class CharacterShow extends Component {
       <>
       <Title />
       <div className='CharShow--wrapper'>
-        <h2>{character.name}</h2>
+        <div className='CharShow--idContainer'>
+          {character.name &&
+            character.thumbnail &&
+          <img
+            className="CharShow--img"
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.name}
+          />}
+          <div className='CharShow--idText'>
+            <h2>{character.name}</h2>
+            {character.description && 
+              <p className='CharShow--description'>{character.description}</p>
+            }
+            <h3 className='CharShow--statTitle'>Statistics</h3>
+            <p className='CharShow--stat'>{character.name} appears in : </p>
+            {character.comics && character.series && character.stories ?
+            
+            <ul>
+              { character.comics.returned ?
+                <li className='CharShow--stats'>{character.comics.returned} comics</li> : null
+              }
+              { character.series.returned ? 
+                <li className='CharShow--stats'>{character.series.returned} series</li> : null
+              }
+              { character.stories.returned ?
+                <li className='CharShow--stats'>{character.stories.returned} stories</li> : null
+              }
+            </ul> : (<h3>No Statistics</h3>)}
+          </div>
+        </div>
+        <div className='CharShow--lists'>
+          <div>
+            <h3>Comics</h3>
+            {character.comics
+              ? character.comics.items.map((item, ind) => <p key={ind}>{item.name} </p>)
+              : null}
+          </div>
+          <div>
+            <h3>Series</h3>
+            {character.series
+              ? character.series.items.map((item, ind) => <p key={ind}>{item.name} </p>)
+              : null}
+          </div>
+          <div>
+            <h3>Stories</h3>  
+            {character.stories
+              ? character.stories.items.map((item, ind) => <p key={ind}>{item.name} </p>)
+              : null}
+          </div>
+        </div>
       </div>
+      <Link to="/">
+        <div className='CharShow--back'>
+          <FontAwesomeIcon className="fas fa-arrow-circle-left" icon={faArrowCircleLeft} />
+          <p>Back home</p>
+        </div>
+      </Link>
       </>
     );
   }
